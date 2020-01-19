e4e2e3e88bb681ae5b649996b0f1dbe963ed5295
Oliver Lippold <olippold@hotmail.com>
2020-01-19 16:42:06 +0000

    Tidied up code

--- HabitTracker/Habit.swift
+++ HabitTracker/Habit.swift
@@ -10,10 +10,6 @@ import Foundation
 
 struct Habit: Identifiable, Codable {
     
-    /*enum CodingKeys: CodingKey {
-        case id, name, description, activityCount
-    } */
-    
     let id = UUID()
     var name: String = ""
     var description: String = ""
@@ -22,25 +18,6 @@ struct Habit: Identifiable, Codable {
     mutating func logHabit() {
         activityCount += 1
     }
-    
-    /*init() { }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        //id = try container.decode(UUID.self, forKey: .id)
-        name = try container.decode(String.self, forKey: .name)
-        description = try container.decode(String.self, forKey: .description)
-        activityCount = try container.decode(Int.self, forKey: .activityCount)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(description, forKey: .description)
-        try container.encode(activityCount, forKey: .activityCount)
-    } */
 }
 
 class Habits: ObservableObject {
@@ -74,18 +51,6 @@ class Habits: ObservableObject {
         
         habits[index].activityCount += 1
     }
-    
-    /*required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        habits = try container.decode([Habit].self, forKey: .habits)
-    }
-    
-    init() { }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(habits, forKey: .habits)
-    } */
     
     func addHabit(_ habit: Habit) {
         habits.append(habit)
